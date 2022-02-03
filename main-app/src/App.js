906c5ef0ed328737cb5f97741d97dc3d5eb5b2c9
Rashid <ralsayan@uwo.ca>
2022-02-02 20:43:00 -0500

    generating function

--- main-app/src/App.js
+++ main-app/src/App.js
@@ -22,4 +22,21 @@ function App() {
   );
 }
 
+function getRandomInt(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min) + min);
+}
+
+function makeArray(size) {
+  const myArray = new Array(size);
+  for (let i = 0; i < myArray.length; i++) {
+    myArray[i] = getRandomInt(1, 20);
+  }
+  return myArray;
+}
+
+let testing = makeArray(10);
+console.log(testing[2]);
+
 export default App;
