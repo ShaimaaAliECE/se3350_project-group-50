9b97d4df0f6467d87fa1c90001fda88047f0f3dd
Kareem <kmeawad@uwo.ca>
2022-02-03 13:33:48 -0500

    Put Level Two in its own file

--- main-app/src/App.js
+++ main-app/src/App.js
@@ -1,29 +1,11 @@
-import Number from "./components/Number";
-import DropBox from "./components/DropBox";
+import LevelTwo from "./components/LevelTwo";
 
 let draggedItem = null;
 
 function App() {
-  let array = makeArray(10);
-  let temp = [];
-  for(let i = 0; i<array.length; i++){
-    temp.push(Number(array[i]))
-  }
   return(
     <div>
-      <table><tbody><tr>{temp}</tr></tbody></table>
-      <table>
-        <tbody>
-        <tr>
-          <DropBox />
-          <DropBox />
-          <DropBox />
-          <DropBox />
-          <DropBox />
-        </tr>
-        </tbody>
-      </table>
-      
+      <LevelTwo />
     </div>
   )
 }
@@ -32,19 +14,7 @@ function setDraggedItem(item){
   draggedItem = item;
 }
 
-function getRandomInt(min, max) {
-  min = Math.ceil(min);
-  max = Math.floor(max);
-  return Math.floor(Math.random() * (max - min) + min);
-}
 
-function makeArray(size) {
-  const myArray = new Array(size);
-  for (let i = 0; i < myArray.length; i++) {
-    myArray[i] = getRandomInt(1, 20);
-  }
-  return myArray;
-}
 
 
 function mergeSort(array) {
