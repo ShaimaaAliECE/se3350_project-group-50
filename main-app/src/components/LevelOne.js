2229e36e528dd2d3214df1a2d10386f687a3c3b0
aalhalb3 <aalhalb3@uwo.ca>
2022-02-03 15:18:48 -0500

    level1

--- main-app/src/components/LevelOne.js
+++ main-app/src/components/LevelOne.js
@@ -1,14 +1,34 @@
 import Number from "./Number";
 import makeArray from "./makeRandomArray";
+import { useState, useEffect } from "react";
+import { TimeoutError } from "bluebird/js/release/errors";
 
 function LevelOne(){
     let array = makeArray(10);
     let temp = [];
+
+    const [step, setStep] = useState(0)
+
     for(let i = 0; i<array.length; i++){
         temp.push(Number(array[i]))
     }
+
+
+    const onClick = () => {
+      // let leftTemp = []
+
+      // for (let i = 0; i < temp.length/2; i++){
+      //   leftTemp.push(temp[i])
+      // }
+
+      setStep(step + 1)
+
+      console.log(step)
+    }
+
     return(
         <div>
+        <button onClick = {onClick}>Next Step</button>
         <table><tbody><tr>{temp}</tr></tbody></table>
         </div>
     )
