7b54dc47a3fbf9a3a4d2005daa24363a73f5c4be
Connor Thomas <cthoma96@uwo.ca>
2022-02-03 14:46:44 -0500

    Database Table

--- main-app/src/SQL/MergeSortScript.sql
+++ main-app/src/SQL/MergeSortScript.sql
@@ -3,3 +3,13 @@ CREATE SCHEMA mergesort;
 
 use mergesort;
 
+CREATE TABLE Customer(
+userID INT NOT NULL AUTO_INCREMENT,
+emailAddress VARCHAR(80) UNIQUE,
+LevelOneTime DOUBLE,
+LevelTwoTime DOUBLE,
+LevelThreeTime DOUBLE,
+LevelFourTime DOUBLE,
+LevelFiveTime DOUBLE,
+LevelReached INT NOT NULL
+);
