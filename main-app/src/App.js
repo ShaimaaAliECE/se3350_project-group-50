fc3188b99c474e407a1ac4a26467ca6d56777155
Kareem <kmeawad@uwo.ca>
2022-02-03 15:42:26 -0500

    Finish most navigation

--- main-app/src/App.js
+++ main-app/src/App.js
@@ -67,6 +67,21 @@ function MergeSortMenu(){
   )
 }
 
+function FuilurePageMenu(){
+  return(
+    <div>
+      You Lost<br />
+      <button onClick= {()=> {ReactDOM.render(<div />, document.getElementById('root'))}}>Replay Level</button>
+      <br />
+      <button onClick= {()=> {ReactDOM.render(<MergeSortMenu/>, document.getElementById('root'))}}>Choose Another Level</button>
+      <br />
+      <button onClick= {()=> {ReactDOM.render(<ChooseAlgoMenu/>, document.getElementById('root'))}}>Pick Algorithm</button>
+      <br />
+      <button onClick= {()=> {ReactDOM.render(<HomePage/>, document.getElementById('root'))}}>Quit</button>
+    </div>
+  )
+}
+
 
 function mergeSort(array) {
   const half = array.length / 2
