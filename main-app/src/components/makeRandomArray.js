d378f67eaa203a87e51b8d07447a63665a6db980
Rashid <ralsayan@uwo.ca>
2022-02-03 15:49:32 -0500

    no repeats

--- main-app/src/components/makeRandomArray.js
+++ main-app/src/components/makeRandomArray.js
@@ -4,12 +4,19 @@ function getRandomInt(min, max) {
     return Math.floor(Math.random() * (max - min) + min);
   }
   
-  function makeArray(size) {
+function makeArray(size) {
     const myArray = new Array(size);
+    const usedNumbers = new Array(size);
     for (let i = 0; i < myArray.length; i++) {
-      myArray[i] = getRandomInt(1, 20);
+      let newNumber = getRandomInt(1, 20);
+      for (let j = 0; j < usedNumbers.length; j++) {
+        while (newNumber == usedNumbers[j]) {
+          newNumber = getRandomInt(1, 20);
+        }
+      }
+      myArray[i] = newNumber;
     }
     return myArray;
-  }
+}
 
   export default makeArray;
