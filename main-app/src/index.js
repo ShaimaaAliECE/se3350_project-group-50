ebd649e978abc5f61e4da3012d0182704a518e8d
aalhalb3 <aalhalb3@uwo.ca>
2022-02-10 16:38:53 -0500

    cookie stuff

--- main-app/src/index.js
+++ main-app/src/index.js
@@ -6,8 +6,14 @@ import reportWebVitals from './reportWebVitals';
 
 const newConnection = require('./SQL/databaseConnection');
 const express = require('express');
+const cookieParser = require('cookie-parser')
 
 const app = express()
+app.use(cookieParser("secret stuff"))
+
+app.use(express.urlencoded({
+  extended: true
+}))
 
 app.get('/login', (req, res) => {
 
