041e1a7c36434ad8879eba590b3d5ef19948038d
Connor Thomas <cthoma96@uwo.ca>
2022-02-18 11:39:35 -0500

    withSTeps

--- main-app/src/components/levels/LevelTwo.js
+++ main-app/src/components/levels/LevelTwo.js
@@ -24,9 +24,9 @@ function LevelTwo(){
     let tempArray4 = []
 
     useEffect(()=>{
-        $("#stepTwo").hide()
-        $("#stepThree").hide()
-        $("#stepFour").hide()
+      //  $("#stepTwo").hide()
+        //$("#stepThree").hide()
+       // $("#stepFour").hide()
         $("#stepFive").hide()
         $("#stepSix").hide()
         $("#stepSeven").hide()
@@ -52,101 +52,101 @@ function LevelTwo(){
         <table style={{marginLeft:"32%"}}><tbody><tr><Numbers array={array}/></tr></tbody></table>
         <table><tbody>{ReactDOM.render(
             <tr>
-            <DropBox key={"1"} divideVal={array[0]} id="1"/>
-            <DropBox key={"2"} divideVal={array[1]} id="2"/>
-            <DropBox key={"3"} divideVal={array[2]} id="3"/>
-            <DropBox key={"4"} divideVal={array[3]} id="4"/>
-            <DropBox key={"5"} divideVal={array[4]} id="5"/>
+            <DropBox key={"1"} divideVal={array[0]} withSteps={true} id="1"/>
+            <DropBox key={"2"} divideVal={array[1]} withSteps={true} id="2"/>
+            <DropBox key={"3"} divideVal={array[2]} withSteps={true} id="3"/>
+            <DropBox key={"4"} divideVal={array[3]} withSteps={true} id="4"/>
+            <DropBox key={"5"} divideVal={array[4]} withSteps={true} id="5"/>
             &nbsp;
             &nbsp;    
-            <DropBox key={"6"} divideVal={array[5]} id="6"/>
-            <DropBox key={"7"} divideVal={array[6]} id="7"/>
-            <DropBox key={"8"} divideVal={array[7]} id="8"/>
-            <DropBox key={"9"} divideVal={array[8]} id="9"/>
-            <DropBox key={"10"} divideVal={array[9]} id="10"/>
+            <DropBox key={"6"} divideVal={array[5]} withSteps={true} id="6"/>
+            <DropBox key={"7"} divideVal={array[6]} withSteps={true} id="7"/>
+            <DropBox key={"8"} divideVal={array[7]} withSteps={true} id="8"/>
+            <DropBox key={"9"} divideVal={array[8]} withSteps={true} id="9"/>
+            <DropBox key={"10"} divideVal={array[9]} withSteps={true} id="10"/>
             </tr>
         , document.getElementById("stepOne"))}
         {
         ReactDOM.render(<tr>
             &nbsp;
             &nbsp;
-            <DropBox key={"11"} divideVal={array[0]} id="11"/>
-            <DropBox key={"12"} divideVal={array[1]} id="12"/>
-            <DropBox key={"13"} divideVal={array[2]} id="13"/>
+            <DropBox key={"11"} divideVal={array[0]} withSteps={true} id="11"/>
+            <DropBox key={"12"} divideVal={array[1]} withSteps={true} id="12"/>
+            <DropBox key={"13"} divideVal={array[2]} withSteps={true} id="13"/>
             &nbsp;
             &nbsp;
-            <DropBox key={"14"} divideVal={array[3]} id="14"/>
-            <DropBox key={"15"} divideVal={array[4]} id="15"/>
+            <DropBox key={"14"} divideVal={array[3]} withSteps={true} id="14"/>
+            <DropBox key={"15"} divideVal={array[4]} withSteps={true} id="15"/>
             &nbsp;
             &nbsp; 
-            <DropBox key={"16"} divideVal={array[5]} id="16"/>
-            <DropBox key={"17"} divideVal={array[6]} id="17"/>
-            <DropBox key={"18"} divideVal={array[7]} id="18"/>
+            <DropBox key={"16"} divideVal={array[5]} withSteps={true} id="16"/>
+            <DropBox key={"17"} divideVal={array[6]} withSteps={true} id="17"/>
+            <DropBox key={"18"} divideVal={array[7]} withSteps={true} id="18"/>
             &nbsp;
             &nbsp;
-            <DropBox key={"19"} divideVal={array[8]} id="19"/>
-            <DropBox key={"20"} divideVal={array[9]} id="20"/> 
+            <DropBox key={"19"} divideVal={array[8]} withSteps={true} id="19"/>
+            <DropBox key={"20"} divideVal={array[9]} withSteps={true} id="20"/> 
             </tr>, document.getElementById("stepTwo"))
         }
         {
             ReactDOM.render(<tr>
             &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-            <DropBox key={"21"} divideVal={array[0]} id="21"/>
-            <DropBox key={"22"} divideVal={array[1]} id="22"/>  
+            <DropBox key={"21"} divideVal={array[0]} withSteps={true} id="21"/>
+            <DropBox key={"22"} divideVal={array[1]} withSteps={true} id="22"/>  
             &nbsp;
             &nbsp;
-            <DropBox key={"23"} divideVal={array[2]} id="23"/> 
+            <DropBox key={"23"} divideVal={array[2]} withSteps={true} id="23"/> 
             &nbsp;
             &nbsp;         
-            <DropBox key={"24"} divideVal={array[3]} id="24"/>
+            <DropBox key={"24"} divideVal={array[3]} withSteps={true} id="24"/>
             &nbsp;
             &nbsp;
-            <DropBox key={"25"} divideVal={array[4]} id="25"/>
+            <DropBox key={"25"} divideVal={array[4]} withSteps={true} id="25"/>
             &nbsp;
             &nbsp;
-            <DropBox key={"26"} divideVal={array[5]} id="26"/>
-            <DropBox key={"27"} divideVal={array[6]} id="27"/>  
+            <DropBox key={"26"} divideVal={array[5]} withSteps={true} id="26"/>
+            <DropBox key={"27"} divideVal={array[6]} withSteps={true} id="27"/>  
             &nbsp;
             &nbsp;
-            <DropBox key={"28"} divideVal={array[7]} id="28"/> 
+            <DropBox key={"28"} divideVal={array[7]} withSteps={true} id="28"/> 
             &nbsp;
             &nbsp;
-            <DropBox key={"29"} divideVal={array[8]} id="29"/>
+            <DropBox key={"29"} divideVal={array[8]} withSteps={true} id="29"/>
             &nbsp;
             &nbsp;
-            <DropBox key={"30"} divideVal={array[9]} id="30"/>
+            <DropBox key={"30"} divideVal={array[9]} withSteps={true} id="30"/>
             </tr>, document.getElementById("stepThree"))
         }
         {
             ReactDOM.render(<tr>
             &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-            <DropBox key={"31"} divideVal={array[0]} id="31"/>
+            <DropBox key={"31"} divideVal={array[0]} withSteps={true} id="31"/>
             &nbsp;
             &nbsp;
-            <DropBox key={"32"} divideVal={array[1]} id="32"/>
+            <DropBox key={"32"} divideVal={array[1]} withSteps={true} id="32"/>
             &nbsp;
             &nbsp;
-            <DropBox key={"33"} divideVal={array[2]} id="33"/> 
+            <DropBox key={"33"} divideVal={array[2]} withSteps={true} id="33"/> 
             &nbsp;
             &nbsp;
-            <DropBox key={"34"} divideVal={array[3]} id="34"/>
+            <DropBox key={"34"} divideVal={array[3]} withSteps={true} id="34"/>
             &nbsp;
             &nbsp;
-            <DropBox key={"35"} divideVal={array[4]} id="35"/>
+            <DropBox key={"35"} divideVal={array[4]} withSteps={true} id="35"/>
             &nbsp;
             &nbsp;
-            <DropBox key={"36"} divideVal={array[5]} id="36"/>
+            <DropBox key={"36"} divideVal={array[5]} withSteps={true} id="36"/>
             &nbsp;
             &nbsp;
-            <DropBox key={"37"} divideVal={array[6]} id="37"/>
+            <DropBox key={"37"} divideVal={array[6]} withSteps={true} id="37"/>
             &nbsp;
             &nbsp;
-            <DropBox key={"38"} divideVal={array[7]} id="38"/> 
+            <DropBox key={"38"} divideVal={array[7]} withSteps={true} id="38"/> 
             &nbsp;
             &nbsp;
-            <DropBox key={"39"} divideVal={array[8]} id="39"/>
+            <DropBox key={"39"} divideVal={array[8]} withSteps={true} id="39"/>
             &nbsp;
-            <DropBox key={"40"} divideVal={array[9]} id="40"/>
+            <DropBox key={"40"} divideVal={array[9]} withSteps={true} id="40"/>
             </tr>, document.getElementById("stepFour"))
         }
         {setTempArray([array[0], array[1]])}
@@ -154,30 +154,30 @@ function LevelTwo(){
         {
             ReactDOM.render(<tr>
             &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-            <DropBox key={"41"} divideVal={tempArray[0]} id="41"/>
-            <DropBox key={"42"} divideVal={tempArray[1]} id="42"/>  
+            <DropBox key={"41"} divideVal={tempArray[0]} withSteps={true} id="41"/>
+            <DropBox key={"42"} divideVal={tempArray[1]} withSteps={true} id="42"/>  
             &nbsp;
             &nbsp;
-            <DropBox key={"43"} divideVal={array[2]} id="43"/> 
+            <DropBox key={"43"} divideVal={array[2]} withSteps={true} id="43"/> 
             &nbsp;
             &nbsp;          
-            <DropBox key={"44"} divideVal={array[3]} id="44"/>
+            <DropBox key={"44"} divideVal={array[3]} withSteps={true} id="44"/>
             &nbsp;
             &nbsp;
-            <DropBox key={"45"} divideVal={array[4]} id="45"/>
+            <DropBox key={"45"} divideVal={array[4]} withSteps={true} id="45"/>
             &nbsp;
             &nbsp;
-            <DropBox key={"46"} divideVal={tempArray2[0]} id="46"/>
-            <DropBox key={"47"} divideVal={tempArray2[1]} id="47"/>  
+            <DropBox key={"46"} divideVal={tempArray2[0]} withSteps={true} id="46"/>
+            <DropBox key={"47"} divideVal={tempArray2[1]} withSteps={true} id="47"/>  
             &nbsp;
             &nbsp;
-            <DropBox key={"48"} divideVal={array[7]} id="48"/> 
+            <DropBox key={"48"} divideVal={array[7]} withSteps={true} id="48"/> 
             &nbsp;
             &nbsp;
-            <DropBox key={"49"} divideVal={array[8]} id="49"/>
+            <DropBox key={"49"} divideVal={array[8]} withSteps={true} id="49"/>
             &nbsp;
             &nbsp;
-            <DropBox key={"50"} divideVal={array[9]} id="50"/>
+            <DropBox key={"50"} divideVal={array[9]} withSteps={true} id="50"/>
             </tr>, document.getElementById("stepFive"))
         }
         {setTempArray([array[0], array[1], array[2]])}
@@ -188,55 +188,55 @@ function LevelTwo(){
         ReactDOM.render(<tr>
             &nbsp;
             &nbsp;
-            <DropBox key={"51"} divideVal={tempArray[0]} id="51"/>
-            <DropBox key={"52"} divideVal={tempArray[1]} id="52"/>
-            <DropBox key={"53"} divideVal={tempArray[2]} id="53"/>
+            <DropBox key={"51"} divideVal={tempArray[0]} withSteps={true} id="51"/>
+            <DropBox key={"52"} divideVal={tempArray[1]} withSteps={true} id="52"/>
+            <DropBox key={"53"} divideVal={tempArray[2]} withSteps={true} id="53"/>
             &nbsp;
             &nbsp;
-            <DropBox key={"54"} divideVal={tempArray2[0]} id="54"/>
-            <DropBox key={"55"} divideVal={tempArray2[1]} id="55"/>
+            <DropBox key={"54"} divideVal={tempArray2[0]} withSteps={true} id="54"/>
+            <DropBox key={"55"} divideVal={tempArray2[1]} withSteps={true} id="55"/>
             &nbsp;
             &nbsp; 
-            <DropBox key={"56"} divideVal={tempArray3[0]} id="56"/>
-            <DropBox key={"57"} divideVal={tempArray3[1]} id="57"/>
-            <DropBox key={"58"} divideVal={tempArray3[2]} id="58"/>
+            <DropBox key={"56"} divideVal={tempArray3[0]} withSteps={true} id="56"/>
+            <DropBox key={"57"} divideVal={tempArray3[1]} withSteps={true} id="57"/>
+            <DropBox key={"58"} divideVal={tempArray3[2]} withSteps={true} id="58"/>
             &nbsp;
             &nbsp;
-            <DropBox key={"59"} divideVal={tempArray4[0]} id="59"/>
-            <DropBox key={"60"} divideVal={tempArray4[1]} id="60"/> 
+            <DropBox key={"59"} divideVal={tempArray4[0]} withSteps={true} id="59"/>
+            <DropBox key={"60"} divideVal={tempArray4[1]} withSteps={true} id="60"/> 
             </tr>, document.getElementById("stepSix"))
         }
         {setTempArray([array[0], array[1], array[2], array[3], array[4]])}
         {setTempArray2([array[5], array[6], array[7], array[8], array[9]])}
         {ReactDOM.render(
             <tr>
-            <DropBox key={"61"} divideVal={tempArray[0]} id="61"/>
-            <DropBox key={"62"} divideVal={tempArray[1]} id="62"/>
-            <DropBox key={"63"} divideVal={tempArray[2]} id="63"/>
-            <DropBox key={"64"} divideVal={tempArray[3]} id="64"/>
-            <DropBox key={"65"} divideVal={tempArray[4]} id="65"/>
+            <DropBox key={"61"} divideVal={tempArray[0]} withSteps={true} id="61"/>
+            <DropBox key={"62"} divideVal={tempArray[1]} withSteps={true} id="62"/>
+            <DropBox key={"63"} divideVal={tempArray[2]} withSteps={true} id="63"/>
+            <DropBox key={"64"} divideVal={tempArray[3]} withSteps={true} id="64"/>
+            <DropBox key={"65"} divideVal={tempArray[4]} withSteps={true} id="65"/>
             &nbsp;
             &nbsp;    
-            <DropBox key={"66"} divideVal={tempArray2[0]} id="66"/>
-            <DropBox key={"67"} divideVal={tempArray2[1]} id="67"/>
-            <DropBox key={"68"} divideVal={tempArray2[2]} id="68"/>
-            <DropBox key={"69"} divideVal={tempArray2[3]} id="69"/>
-            <DropBox key={"70"} divideVal={tempArray2[4]} id="70"/>
+            <DropBox key={"66"} divideVal={tempArray2[0]} withSteps={true} id="66"/>
+            <DropBox key={"67"} divideVal={tempArray2[1]} withSteps={true} id="67"/>
+            <DropBox key={"68"} divideVal={tempArray2[2]} withSteps={true} id="68"/>
+            <DropBox key={"69"} divideVal={tempArray2[3]} withSteps={true} id="69"/>
+            <DropBox key={"70"} divideVal={tempArray2[4]} withSteps={true} id="70"/>
             </tr>
         , document.getElementById("stepSeven"))}
         {setTempArray([...array])}
         {ReactDOM.render(
             <tr>
-            <DropBox key={"71"} divideVal={tempArray[0]} id="71"/>
-            <DropBox key={"72"} divideVal={tempArray[1]} id="72"/>
-            <DropBox key={"73"} divideVal={tempArray[2]} id="73"/>
-            <DropBox key={"74"} divideVal={tempArray[3]} id="74"/>
-            <DropBox key={"75"} divideVal={tempArray[4]} id="75"/> 
-            <DropBox key={"76"} divideVal={tempArray[5]} id="76"/>
-            <DropBox key={"77"} divideVal={tempArray[6]} id="77"/>
-            <DropBox key={"78"} divideVal={tempArray[7]} id="78"/>
-            <DropBox key={"79"} divideVal={tempArray[8]} id="79"/>
-            <DropBox key={"80"} divideVal={tempArray[9]} id="80"/>
+            <DropBox key={"71"} divideVal={tempArray[0]} withSteps={true} id="71"/>
+            <DropBox key={"72"} divideVal={tempArray[1]} withSteps={true} id="72"/>
+            <DropBox key={"73"} divideVal={tempArray[2]} withSteps={true} id="73"/>
+            <DropBox key={"74"} divideVal={tempArray[3]} withSteps={true} id="74"/>
+            <DropBox key={"75"} divideVal={tempArray[4]} withSteps={true} id="75"/> 
+            <DropBox key={"76"} divideVal={tempArray[5]} withSteps={true} id="76"/>
+            <DropBox key={"77"} divideVal={tempArray[6]} withSteps={true} id="77"/>
+            <DropBox key={"78"} divideVal={tempArray[7]} withSteps={true} id="78"/>
+            <DropBox key={"79"} divideVal={tempArray[8]} withSteps={true} id="79"/>
+            <DropBox key={"80"} divideVal={tempArray[9]} withSteps={true} id="80"/>
             </tr>
         , document.getElementById("stepEight"))}
         </tbody>
