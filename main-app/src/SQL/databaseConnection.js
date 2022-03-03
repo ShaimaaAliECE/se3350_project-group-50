9b5afd3f6a32ab2f32c62543cd7c8a0a0adab4b4
Rashid <ralsayan@uwo.ca>
2022-03-03 15:09:30 -0500

    updated ip

--- main-app/src/SQL/databaseConnection.js
+++ main-app/src/SQL/databaseConnection.js
@@ -2,7 +2,7 @@ const mysql = require('mysql');
 
 function establishConnection(){
     let connection = mysql.createConnection({
-        host: '104.155.146.37',
+        host: '34.133.136.39',
         user: 'root',
         password: 'password',
         database: 'projectDB'
