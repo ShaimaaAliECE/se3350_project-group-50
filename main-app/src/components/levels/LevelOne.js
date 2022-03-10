84439cf76bf3b695729e2bfb1fe273847dfcf502
khdag <khanhchel2001@gmail.com>
2022-03-10 17:44:40 -0500

    change

--- main-app/src/components/levels/LevelOne.js
+++ main-app/src/components/levels/LevelOne.js
@@ -131,7 +131,7 @@ function LevelOne(){
   //Display timer
   const [seconds, setSeconds] = useState(0);
   const [minutes, setMinutes] = useState(0);
-  const [isActive, setIsActive] = useState(true);
+  const isActive = useState(true);
 
   useEffect(() => {
     let interval = null;
