ad067bb9ab4e7323925d6f91227475ccc386168f
Rashid <ralsayan@uwo.ca>
2022-03-10 18:18:05 -0500

    syntax fix

--- main-app/src/App.js
+++ main-app/src/App.js
@@ -148,7 +148,6 @@ function MergeSortMenu(){
           <button onClick= {()=> {ReactDOM.render(<ChooseAlgoMenu/>, document.getElementById('root'))}}>Back</button>
         </div>
       )
-      break;
     case 2:
       return(
         <div>
@@ -160,7 +159,6 @@ function MergeSortMenu(){
           <button onClick= {()=> {ReactDOM.render(<ChooseAlgoMenu/>, document.getElementById('root'))}}>Back</button>
         </div>
       )
-      break;
     case 3:
       return(
         <div>
@@ -174,7 +172,6 @@ function MergeSortMenu(){
           <button onClick= {()=> {ReactDOM.render(<ChooseAlgoMenu/>, document.getElementById('root'))}}>Back</button>
         </div>
       )
-      break;
     case 4:
       return(
         <div>
@@ -190,7 +187,6 @@ function MergeSortMenu(){
           <button onClick= {()=> {ReactDOM.render(<ChooseAlgoMenu/>, document.getElementById('root'))}}>Back</button>
         </div>
       )
-      break;
     case 5:
       return(
         <div>
@@ -208,24 +204,7 @@ function MergeSortMenu(){
           <button onClick= {()=> {ReactDOM.render(<ChooseAlgoMenu/>, document.getElementById('root'))}}>Back</button>
         </div>
       )
-      break;
   }
-  return(
-    <div>
-      <h1>Choose Level</h1>
-      <button onClick= {()=> {ReactDOM.render(<LevelOne/>, document.getElementById('root'))}}>Level One</button>
-      <br />
-      <button onClick= {()=> {ReactDOM.render(<LevelTwo/>, document.getElementById('root'))}}>Level Two</button>
-      <br />
-      <button onClick= {()=> {ReactDOM.render(<LevelThree/>, document.getElementById('root'))}}>Level Three</button>
-      <br />
-      <button onClick= {()=> {ReactDOM.render(<LevelFour/>, document.getElementById('root'))}}>Level Four</button>
-      <br />
-      <button onClick= {()=> {ReactDOM.render(<LevelFive/>, document.getElementById('root'))}}>Level Five</button>
-      <br/>
-      <button onClick= {()=> {ReactDOM.render(<ChooseAlgoMenu/>, document.getElementById('root'))}}>Back</button>
-    </div>
-  )
 }
 
 function mergeSort(array) {
