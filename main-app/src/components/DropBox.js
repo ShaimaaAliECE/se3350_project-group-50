548f12dc60965cf680a8142a5d821f8f7bd8025a
MoeStyll <mamine2@uwo.ca>
2022-03-11 20:09:48 -0500

    feedback

--- main-app/src/components/DropBox.js
+++ main-app/src/components/DropBox.js
@@ -7,9 +7,10 @@ import sike from '../Sike.mp3'
 import ding from '../Ding.mp3'
 import FailurePageMenu from "./FailurePageMenu";
 
-const dumb = ["Are you dumb?", "Wow, you really did something here.", "The light is on but nobody's home.", "Has an IQ of 2 and it takes 3 to grunt", "Did you get a nobel prize yet?", "The wheel is spinning but the hamster's dead", "Bruh", "You are just bring silly now.", "Stop monkeying around to see if it works."]
 
 const steps = ["Split the Array as Evenly as Possible by dragging the numbers given in the corresponding empty box", "Split Both the Left and Right Subarrays as evenly as possible","Split All Subarrays again","Split Subarrays that have more than one element", "Merge both Arrays Back Together in the Correct Order", "Merge those Subarrays back together in the Correct Order Again", "Merge the Subarrays Again","Finally, Merge the Remaining Subarrays Back Into the Original Array, Now its Sorted"]
+
+const dumb = [" Are you dumb?", " Wow, you really did something here.", " The light is on but nobody's home.", " Has an IQ of 2 and it takes 3 to grunt", " Did you get a nobel prize yet?", " The wheel is spinning but the hamster's dead", " Bruh", " You are just bring silly now.", " Stop monkeying around to see if it works."]
 
 //component to drop numbers in 
 function DropBox(props){
@@ -27,35 +28,43 @@ function DropBox(props){
             if(getScore() == 10){
                 $("#stepTwo").show();
                 document.getElementById("stepsTutorial").innerText = steps[1];
+                document.getElementById('stepsTutorial').innerText += dumb[0];
             }
             if(getScore() == 20){
                 $("#stepThree").show();
                 document.getElementById("stepsTutorial").innerText = steps[2];
+                document.getElementById('stepsTutorial').innerText = dumb[1];
             }
             if(getScore() == 30){
                 $("#stepFour").show();
-                document.getElementById("stepsTutorial").innerText = steps[3];   
+                document.getElementById("stepsTutorial").innerText = steps[3];  
+                document.getElementById('stepsTutorial').innerText = dumb[2]; 
             } 
             if(getScore() == 40){
                 $("#stepFive").show();
-                document.getElementById("stepsTutorial").innerText = steps[4];   
+                document.getElementById("stepsTutorial").innerText = steps[4];
+                document.getElementById('stepsTutorial').innerText = dumb[3];   
             }
             if(getScore() == 50){
                 $("#stepSix").show();
                 document.getElementById("stepsTutorial").innerText = steps[5];   
+                document.getElementById('stepsTutorial').innerText = dumb[4];
             }
             if(getScore() == 60){
                 $("#stepSeven").show();
                 document.getElementById("stepsTutorial").innerText = steps[6];   
+                document.getElementById('stepsTutorial').innerText = dumb[5];
             }
             if(getScore() == 70){
                 $("#stepEight").show();
                 document.getElementById("stepsTutorial").innerText = steps[7];   
+                document.getElementById('stepsTutorial').innerText = dumb[6];
             }
             if(getScore() == 80){
                 setScore(0)
                 $("#nextBtn").show();
-                document.getElementById("stepsTutorial").innerText = steps[8];   
+                document.getElementById("stepsTutorial").innerText = steps[8];  
+                document.getElementById('stepsTutorial').innerText = dumb[7]; 
             }
            
             //Audio feedback for correct goes here
