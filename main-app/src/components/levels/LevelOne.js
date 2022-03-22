a8e76349ac0f155ce8c1cfb2d35708f6d66efb10
Connor Thomas <cthoma96@uwo.ca>
2022-03-22 15:19:55 -0400

    LevelOne

--- main-app/src/components/levels/LevelOne.js
+++ main-app/src/components/levels/LevelOne.js
@@ -1,5 +1,5 @@
 import Number from "../Number";
-import { App, mergeSort, emailEntered } from "../../App";
+import { App, emailEntered } from "../../App";
 import Numbers from "../Numbers";
 import makeArray from "../makeRandomArray";
 import { useState, useEffect } from "react";
@@ -8,6 +8,37 @@ import {LevelTwo} from "./LevelTwo";
 import axios from "axios";
 import { MergeSortMenu } from "../../App";
 let Timer = require('../Timer');
+
+
+function mergeSort(array) {
+  const half = array.length / 2
+  
+  // Base case or terminating case
+  if(array.length < 2){
+    return array 
+  }
+  
+  const left = array.splice(0, half)
+  return merge(mergeSort(left),mergeSort(array))
+}
+
+//function for merging on left and right
+function merge(left, right) {
+  let arr = []
+  // Break out of loop if any one of the array gets empty
+  while (left.length && right.length) {
+      // Pick the smaller among the smallest element of left and right sub arrays 
+      if (left[0] < right[0]) {
+          arr.push(left.shift())  
+      } else {
+          arr.push(right.shift()) 
+      }
+  }
+  
+  // Concatenating the leftover elements
+  // (in case we didn't go through the entire left or right array)
+  return [ ...arr, ...left, ...right ]
+}
 
 
 function LevelOne(){
