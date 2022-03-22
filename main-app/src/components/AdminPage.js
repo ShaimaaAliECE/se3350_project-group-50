2c4294334230dfcfbf63230400fb60333c530133
MoeStyll <mamine2@uwo.ca>
2022-03-22 15:14:02 -0400

    adding screen time

--- main-app/src/components/AdminPage.js
+++ main-app/src/components/AdminPage.js
@@ -1,12 +1,23 @@
+const Stack = createStackNavigator();
+const Tab = createMaterialTopTabNavigator();
+export default function AdminPage(props){
+const { navigation } = props;
+const pathway_id=navigation.getParam('pathway_id');
 
 //Page for admin to view graph
-function AdminPage(props){
     return(
         <>
-            Graph Here
-        </>
-    )
+    <NavigationContainer>
+      <Tab.Navigator tabBarOptions={{ scrollEnabled: true }}>
+      <Tab.Screen name="Course" component={Courses} initialParams={{pathway_id:pathway_id}} />
+        <Tab.Screen name="Roadmap" component={Roadmap} />
+      </Tab.Navigator>
+    </NavigationContainer>
+    </>
+  );
+
+  
 }
 
 
-export default AdminPage;
+
